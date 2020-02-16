728c555a21c4b5679cb1a3073f49c118b0935957
Kirazy <35714562+kirazy@users.noreply.github.com>
2020-02-16 01:34:44 -0800

    Additional entities

---
+++ data-updates.lua
@@ -0,0 +1,6 @@
+-- Copyright (c) 2020 Kirazy
+-- Part of Reskin Series: Bob's Structures
+--     
+-- See LICENSE.md in the project directory for license information.
+
+require("prototypes.entity.assembling-machine")
