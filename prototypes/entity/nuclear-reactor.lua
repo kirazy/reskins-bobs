905efb76eb6285f6ece29b6ed4a1c5009d306cc7
Kirazy <35714562+kirazy@users.noreply.github.com>
2020-02-19 00:04:08 -0800

    Update nuclear-reactor.lua

--- prototypes/entity/nuclear-reactor.lua
+++ prototypes/entity/nuclear-reactor.lua
@@ -1,3 +1,8 @@
+-- Copyright (c) 2020 Kirazy
+-- Part of Reskin Series: Bob's Structures
+--     
+-- See LICENSE.md in the project directory for license information.
+
 -- Check to see if reskinning needs to be done.
 if not mods["bobpower"] then return end
 if settings.startup["bobmods-power-nuclear"].value == false then return end
