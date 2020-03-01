709de1cebb67eb3892e80f0127af32496dd49508
Kirazy <35714562+kirazy@users.noreply.github.com>
2020-02-29 23:40:41 -0800

    Electrolyser, mining drill image population

--- data.lua
+++ data.lua
@@ -28,3 +28,31 @@ require("prototypes.entity.storage-tank")
 -- Bob's Warfare
 require("prototypes.entity.gun-turret")
 
+-- Mining Drill Animation Test
+-- data.raw["mining-drill"]["electric-mining-drill"]["animations"]["east"] = 
+-- {
+--     priority = "high",
+--     filename = "__base__/graphics/entity/electric-mining-drill/electric-mining-drill-E.png",
+--     line_length = 8,
+--     width = 105,
+--     height = 98,
+--     frame_count = 32,
+--     animation_speed = 0.5,
+--     direction_count = 1,
+--     shift = util.by_pixel(3.5, -1),
+--     run_mode = "forward-then-backward",
+--     hr_version =
+--     {
+--         priority = "high",
+--         filename = reskins.bobs_structures.directory.."/graphics/entity/electric-mining-drill/test/large-miner-test.png",
+--         line_length = 8,
+--         width = 211,
+--         height = 197,
+--         frame_count = 32,
+--         animation_speed = 0.5,
+--         direction_count = 1,
+--         shift = util.by_pixel(3.75, -1.25),
+--         run_mode = "forward-then-backward",
+--         scale = 0.5
+--     }
+-- }
