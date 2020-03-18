cf71cf529257340867092542540b3693b0434d8a
Kirazy <35714562+kirazy@users.noreply.github.com>
2020-03-18 14:41:21 -0700

    the rest of the robots

---
+++ prototypes/entity/logistics/construction-robots.lua
@@ -0,0 +1,11 @@
+
+
+
+local tier_map =
+{
+    ["construction-robot"]       = {1},
+    ["bob-construction-robot-2"] = {2},
+    ["bob-construction-robot-3"] = {3},
+    ["bob-construction-robot-4"] = {4},
+    ["bob-construction-robot-5"] = {5},
+}
