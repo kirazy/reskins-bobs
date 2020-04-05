a72fcd1a010a5759682eb0fc32916faee03471b4
Kirazy <35714562+kirazy@users.noreply.github.com>
2020-04-05 15:43:28 -0700

    Cleanup

--- data.lua
+++ data.lua
@@ -39,7 +39,7 @@ require("prototypes.entity.logistics.valve")
 
 -- Bob's Mining
 require("prototypes.entity.mining.mining-drill")
-require("prototypes.entity.mining.pumpjack")
+-- require("prototypes.entity.mining.pumpjack") -- THIS ENTRY IS NOT COMPLETE IN STANDARD RESOLUTION
 
 -- Bob's Modules
 -- require("prototypes.entity.modules.beacon")
@@ -65,7 +65,7 @@ require("prototypes.entity.power.steam-turbine")
 require("prototypes.entity.power.substation")
 
 -- Bob's Technology
-require("prototypes.entity.technology.lab")
+-- require("prototypes.entity.technology.lab") -- THIS ENTRY IS NOT COMPLETE IN STANDARD RESOLUTION
 
 -- Bob's Warfare
 -- require("prototypes.entity.warfare.artillery-turret")
