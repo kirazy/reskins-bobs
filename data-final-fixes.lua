45c14845eac091a0635eb4fe5f3c8f65429b1c78
Kirazy <35714562+kirazy@users.noreply.github.com>
2020-07-24 02:04:56 -0700

    Consistent styling

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -3,20 +3,27 @@
 --     
 -- See LICENSE.md in the project directory for license information.
 
--- Technology
+----------------------------------------------------------------------------------------------------
+-- TECHNOLOGIES
+----------------------------------------------------------------------------------------------------
 require("prototypes.technology.logistics")
 require("prototypes.technology.power")
 
--- Icon reassignments
+----------------------------------------------------------------------------------------------------
+-- DEFERRED ASSIGNMENTS
+----------------------------------------------------------------------------------------------------
+-- Item Icons
 for name, inputs in pairs(reskins.bobs.icons) do
     reskins.lib.assign_icons(name, inputs)
 end
 
--- Technology reassignments
+-- Technology Icons
 for name, inputs in pairs(reskins.bobs.technology) do
     reskins.lib.assign_technology_icons(name, inputs)
 end
 
--- Compatibility
+----------------------------------------------------------------------------------------------------
+-- COMPATIBILITY
+----------------------------------------------------------------------------------------------------
 require("prototypes.compatibility.mini-machines") -- This must be called after icons are handled
 require("prototypes.compatibility.deadlock-crating") -- DeadlockCrating sets up machines in data-final-fixes
