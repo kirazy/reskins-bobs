a478f7c9e6b5f63da8eaedbe5c46c18c717ed2da
Kirazy <35714562+kirazy@users.noreply.github.com>
2020-07-31 18:52:52 -0700

    bearings, gears, ball-bearings, remaining plates

--- prototypes/item/plates.lua
+++ prototypes/item/plates.lua
@@ -17,48 +17,71 @@ local inputs = {
 reskins.lib.parse_inputs(inputs)
 
 -- Metal plates
-local plate_inputs = util.copy(inputs)
+local intermediaries_inputs = util.copy(inputs)
 
-local metal_plates = {
-    "aluminium-plate",
-    "brass-alloy",
-    "bronze-alloy",
-    "cobalt-plate",
-    "cobalt-steel-alloy",
-    "copper-tungsten-alloy",
-    "gold-plate",
-    -- "gunmetal-plate",
-    -- "invar-plate",
-    "lead-plate",
-    "lithium",
-    "nickel-plate",
-    "nitinol-alloy",
-    -- "silicon-plate",
-    -- "silver-plate",
-    -- "solder-plate",
-    -- "steel-plate",
-    "tin-plate",
-    "titanium-plate",
-    -- "tungsten-carbide-plate",
-    "tungsten-plate",
-    -- "zinc-plate",
-    "alien-orange-alloy",
-    "alien-blue-alloy",
+local intermediaries = {
+    -- Plates
+    ["aluminium-plate"] = "plates",
+    ["brass-alloy"] = "plates",
+    ["bronze-alloy"] = "plates",
+    ["cobalt-plate"] = "plates",
+    ["cobalt-steel-alloy"] = "plates",
+    ["copper-tungsten-alloy"] = "plates",
+    ["gold-plate"] = "plates",
+    ["gunmetal-alloy"] = "plates",
+    ["invar-alloy"] = "plates",
+    ["lead-plate"] = "plates",
+    ["lithium"] = "plates",
+    ["nickel-plate"] = "plates",
+    ["nitinol-alloy"] = "plates",
+    -- ["silicon-plate"] = "plates",
+    ["silver-plate"] = "plates",
+    ["solder-alloy"] = "plates",
+    ["tin-plate"] = "plates",
+    ["titanium-plate"] = "plates",
+    ["tungsten-carbide"] = "plates",
+    ["tungsten-plate"] = "plates",
+    ["zinc-plate"] = "plates",
+    ["alien-orange-alloy"] = "plates",
+    ["alien-blue-alloy"] = "plates",
+
+    -- Bearings
+    ["ceramic-bearing"] = "bearings",
+    ["cobalt-steel-bearing"] = "bearings",
+    ["nitinol-bearing"] = "bearings",
+    ["steel-bearing"] = "bearings",
+    ["titanium-bearing"] = "bearings",
+
+    -- Bearing Balls
+    ["ceramic-bearing-ball"] = "bearing-balls",
+    ["cobalt-steel-bearing-ball"] = "bearing-balls",
+    ["nitinol-bearing-ball"] = "bearing-balls",
+    ["steel-bearing-ball"] = "bearing-balls",
+    ["titanium-bearing-ball"] = "bearing-balls",
+    
+    -- Gear Wheels
+    ["brass-gear-wheel"] = "gears",
+    ["cobalt-steel-gear-wheel"] = "gears",
+    ["nitinol-gear-wheel"] = "gears",
+    ["steel-gear-wheel"] = "gears",
+    ["titanium-gear-wheel"] = "gears",
+    ["tungsten-gear-wheel"] = "gears",
 }
 
-for _, name in pairs(metal_plates) do
+for name, subfolder in pairs(intermediaries) do
     -- Fetch item
     local item = data.raw.item[name]
 
     -- Check if item exists, if not, skip this iteration
     if not item then goto continue end
 
-    plate_inputs.icon_filename = plate_inputs.directory.."/graphics/icons/plates/plates/"..name..".png"
+    intermediaries_inputs.icon_filename = intermediaries_inputs.directory.."/graphics/icons/plates/"..subfolder.."/"..name..".png"
 
-    reskins.lib.construct_icon(name, 0, plate_inputs)
+    reskins.lib.construct_icon(name, 0, intermediaries_inputs)
 
+    -- One-off fixes
     if name == "nickel-plate" then
-        -- One-off fixes
+        
         data.raw.recipe["bob-nickel-plate"].icon = nil
         data.raw.recipe["bob-nickel-plate"].icon_size = nil
         data.raw.recipe["bob-nickel-plate"].icon_mipmaps = nil
@@ -68,8 +91,6 @@ for _, name in pairs(metal_plates) do
     -- Label to skip to next iteration
     ::continue::
 end
-
-
 
 local dual_product_recipes = {
     ["cobalt-oxide-from-copper"] = {"copper-plate", "cobalt-oxide"},
