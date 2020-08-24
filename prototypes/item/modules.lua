b96468f9843c9642e473fb5b6479b51e2a6529e7
Kirazy <35714562+kirazy@users.noreply.github.com>
2020-08-24 10:40:02 -0700

    Fix modules when CP module override setting is uncommented and turned off

--- prototypes/item/modules.lua
+++ prototypes/item/modules.lua
@@ -1,11 +1,12 @@
 -- Copyright (c) 2020 Kirazy
 -- Part of Artisanal Reskins: Bob's Mods
---     
+--
 -- See LICENSE.md in the project directory for license information.
 
 -- Check to see if reskinning needs to be done.
 if not mods["bobmodules"] then return end
-if mods["CircuitProcessing"] then return end
+if reskins.lib.setting("cp-override-modules") == false then -- Do nothing
+elseif mods["CircuitProcessing"] then return end
 if reskins.lib.setting("reskins-bobs-do-bobmodules") == false then return end
 
 -- Modules
