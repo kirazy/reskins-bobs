7b95937201c0d497969268d3e49e62c6ea0fb246
Kirazy <35714562+kirazy@users.noreply.github.com>
2020-10-19 03:18:20 -0700

    locale tweaks for consistency, vanilla-pipe override respects angel-specific pipe override

--- prototypes/entity/logistics/pipe.lua
+++ prototypes/entity/logistics/pipe.lua
@@ -65,7 +65,7 @@ local function reskin_pipe_entity()
 end
 
 -- Check to see if reskinning needs to be done
-if not mods["boblogistics"] then return else reskin_pipe_entity() end
+if not mods["boblogistics"] then return elseif not mods["reskins-angels"] then reskin_pipe_entity() end
 if reskins.lib.setting("reskins-bobs-do-boblogistics") == false then return end
 
 -- Set input parameters
