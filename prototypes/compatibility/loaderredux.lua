22af43f28e4eda10953d8c68e6105d649a1f32e1
Kirazy <35714562+kirazy@users.noreply.github.com>
2020-11-18 23:56:09 -0800

    Add support for forthcoming loader redux update

--- prototypes/compatibility/loaderredux.lua
+++ prototypes/compatibility/loaderredux.lua
@@ -9,6 +9,13 @@ if mods["vanilla-loaders-hd"] then return end
 if not mods["boblogistics"] then return end
 if reskins.lib.setting("reskins-bobs-do-boblogistics") == false then return end
 
+-- Check if we're using the re-rendered graphics in LoaderRedux
+local migration, using_new_graphics
+if mods["flib"] then -- LoaderRedux requires flib as a dependency, but just in case
+    migration = require("__flib__.migration")
+    using_new_graphics = migration.is_newer_version("1.5.3", mods["LoaderRedux"])
+end
+
 -- Set input parameters
 local inputs = {
     type = "loader",
@@ -16,7 +23,6 @@ local inputs = {
     base_entity = "splitter",
     mod = "bobs",
     group = "compatibility",
-    subgroup = "loaderredux",
     particles = {["medium"] = 1, ["big"] = 4},
     icon_layers = 2,
     make_remnants = false,
@@ -28,11 +34,11 @@ if reskins.lib.setting("reskins-bobs-do-belt-entity-tier-labeling") == true then
 end
 
 local tier_map = {
-    ["loader"] = {1, 1},
-    ["fast-loader"] = {2, 2},
-    ["express-loader"] = {3, 2,},
-    ["purple-loader"] = {4, 2},
-    ["green-loader"] = {5, 2},
+    ["loader"] = {tier = 1, variant = 1},
+    ["fast-loader"] = {tier = 2, variant = 2},
+    ["express-loader"] = {tier = 3, variant = 2,},
+    ["purple-loader"] = {tier = 4, variant = 2},
+    ["green-loader"] = {tier = 5, variant = 2},
 }
 
 -- Reskin entities
@@ -41,91 +47,99 @@ for name, map in pairs(tier_map) do
     local entity = data.raw[inputs.type][name]
 
     -- Check if entity exists, if not, skip this iteration
-    if not entity then
-        goto continue
-    end
-
-    -- Parse map
-    local tier = map[1]
-    local variant = map[2]
+    if not entity then goto continue end
 
     -- Determine what tint we're using
-    inputs.tint = reskins.bobs.belt_tint_handling(name, tier)
+    inputs.tint = reskins.bobs.belt_tint_handling(name, map.tier)
 
-    reskins.lib.setup_standard_entity(name, tier, inputs)
+    -- Specify the icon we're using
+    if using_new_graphics then
+        inputs.subgroup = "loaderredux/new"
+    else
+        inputs.subgroup = "loaderredux/old"
+    end
+
+    reskins.lib.setup_standard_entity(name, map.tier, inputs)
 
     -- Retint the entity mask
-    entity.structure.direction_in.sheets = {
-        -- Base
-        {
-            filename = reskins.bobs.directory.."/graphics/entity/compatibility/loaderredux/loader/loader-base.png",
-            priority = "extra-high",
-            width = 128,
-            height = 128,
-            hr_version = {
-                filename = reskins.bobs.directory.."/graphics/entity/compatibility/loaderredux/loader/hr-loader-base.png",
+    if using_new_graphics then
+        entity.structure.direction_in.sheets[2].tint = inputs.tint
+        entity.structure.direction_in.sheets[2].hr_version.tint = inputs.tint
+        entity.structure.direction_out.sheets[2].tint = inputs.tint
+        entity.structure.direction_out.sheets[2].hr_version.tint = inputs.tint
+    else
+        entity.structure.direction_in.sheets = {
+            -- Base
+            {
+                filename = reskins.bobs.directory.."/graphics/entity/compatibility/loaderredux/loader/loader-base.png",
                 priority = "extra-high",
-                width = 256,
-                height = 256,
-                scale = 0.5,
-            }
-        },
-        -- Mask
-        {
-            filename = reskins.bobs.directory.."/graphics/entity/compatibility/loaderredux/loader/loader-mask.png",
-            priority = "extra-high",
-            width = 128,
-            height = 128,
-            tint = inputs.tint,
-            hr_version = {
-                filename = reskins.bobs.directory.."/graphics/entity/compatibility/loaderredux/loader/hr-loader-mask.png",
+                width = 128,
+                height = 128,
+                hr_version = {
+                    filename = reskins.bobs.directory.."/graphics/entity/compatibility/loaderredux/loader/hr-loader-base.png",
+                    priority = "extra-high",
+                    width = 256,
+                    height = 256,
+                    scale = 0.5,
+                }
+            },
+            -- Mask
+            {
+                filename = reskins.bobs.directory.."/graphics/entity/compatibility/loaderredux/loader/loader-mask.png",
                 priority = "extra-high",
-                width = 256,
-                height = 256,
+                width = 128,
+                height = 128,
                 tint = inputs.tint,
-                scale = 0.5,
+                hr_version = {
+                    filename = reskins.bobs.directory.."/graphics/entity/compatibility/loaderredux/loader/hr-loader-mask.png",
+                    priority = "extra-high",
+                    width = 256,
+                    height = 256,
+                    tint = inputs.tint,
+                    scale = 0.5,
+                }
             }
         }
-    }
-    entity.structure.direction_out.sheets = {
-        -- Base
-        {
-            filename = reskins.bobs.directory.."/graphics/entity/compatibility/loaderredux/loader/loader-base.png",
-            priority = "extra-high",
-            width = 128,
-            height = 128,
-            y = 128,
-            hr_version = {
-                filename = reskins.bobs.directory.."/graphics/entity/compatibility/loaderredux/loader/hr-loader-base.png",
+        entity.structure.direction_out.sheets = {
+            -- Base
+            {
+                filename = reskins.bobs.directory.."/graphics/entity/compatibility/loaderredux/loader/loader-base.png",
                 priority = "extra-high",
-                width = 256,
-                height = 256,
-                y = 256,
-                scale = 0.5,
-            }
-        },
-        -- Mask
-        {
-            filename = reskins.bobs.directory.."/graphics/entity/compatibility/loaderredux/loader/loader-mask.png",
-            priority = "extra-high",
-            width = 128,
-            height = 128,
-            y = 128,
-            tint = inputs.tint,
-            hr_version = {
-                filename = reskins.bobs.directory.."/graphics/entity/compatibility/loaderredux/loader/hr-loader-mask.png",
+                width = 128,
+                height = 128,
+                y = 128,
+                hr_version = {
+                    filename = reskins.bobs.directory.."/graphics/entity/compatibility/loaderredux/loader/hr-loader-base.png",
+                    priority = "extra-high",
+                    width = 256,
+                    height = 256,
+                    y = 256,
+                    scale = 0.5,
+                }
+            },
+            -- Mask
+            {
+                filename = reskins.bobs.directory.."/graphics/entity/compatibility/loaderredux/loader/loader-mask.png",
                 priority = "extra-high",
-                width = 256,
-                height = 256,
-                y = 256,
+                width = 128,
+                height = 128,
+                y = 128,
                 tint = inputs.tint,
-                scale = 0.5,
+                hr_version = {
+                    filename = reskins.bobs.directory.."/graphics/entity/compatibility/loaderredux/loader/hr-loader-mask.png",
+                    priority = "extra-high",
+                    width = 256,
+                    height = 256,
+                    y = 256,
+                    tint = inputs.tint,
+                    scale = 0.5,
+                }
             }
         }
-    }
+    end
 
     -- Apply belt set
-    entity.belt_animation_set = reskins.bobs.transport_belt_animation_set(inputs.tint, variant)
+    entity.belt_animation_set = reskins.bobs.transport_belt_animation_set(inputs.tint, map.variant)
 
     -- Label to skip to next iteration
     ::continue::
