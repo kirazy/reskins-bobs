defb4b5a3a7770f3c64b6ab9754f543ccad14e8a
Kira <35714562+kirazy@users.noreply.github.com>
2021-04-14 16:06:06 -0700

    Disable setting override for bob-revamp

--- settings-updates.lua
+++ settings-updates.lua
@@ -4,4 +4,4 @@
 -- See LICENSE in the project directory for license information.
 
 -- Override Bob's Revamp
-reskins.lib.setting_override("bool-setting", "reskins-bobs-do-bobrevamp", true)
+-- reskins.lib.setting_override("bool-setting", "reskins-bobs-do-bobrevamp", true)
