882ea5b277b532b1f5096f9754f372b1c276f16c
Kira <35714562+kirazy@users.noreply.github.com>
2021-06-11 13:34:34 -0700

    Add capacity effects to tech robot storage

--- prototypes/technology/technology.lua
+++ prototypes/technology/technology.lua
@@ -25,6 +25,33 @@ local technologies = {
     -- ["lab"] = {}, -- lab 1
     -- ["advanced-research"] = {}, -- lab 2
     -- ["alien-research"] = {}, -- alien science packs, alien lab
+
+    -- TECHNOLOGY EFFECTS
+    ["bob-infinite-worker-robots-storage-1"] = {technology_icon_filename = "__base__/graphics/technology/worker-robots-storage.png", technology_icon_extras = {reskins.lib.return_technology_effect_icon("capacity")}, technology_icon_size = 256, technology_icon_mipmaps = 4, flat_icon = true},
 }
 
 reskins.lib.create_icons_from_list(technologies, inputs)
+
+-- Overwrite icons for technology effects
+data.raw["utility-sprites"].default.worker_robot_storage_modifier_icon = util.merge{data.raw["utility-sprites"].default.worker_robot_storage_modifier_icon, {
+    filename = "__base__/graphics/icons/logistic-robot.png",
+    width = 64,
+    height = 64,
+    mipmap_count = 4,
+}}
+
+data.raw["utility-sprites"].default.worker_robot_speed_modifier_icon = util.merge{data.raw["utility-sprites"].default.worker_robot_speed_modifier_icon, {
+    filename = "__base__/graphics/icons/logistic-robot.png",
+    width = 64,
+    height = 64,
+    mipmap_count = 4,
+}}
+
+-- Set infer_icon properties for associated technologies
+data.raw.technology["worker-robots-storage-1"].effects[1].infer_icon = false
+data.raw.technology["worker-robots-storage-2"].effects[1].infer_icon = false
+data.raw.technology["worker-robots-storage-3"].effects[1].infer_icon = false
+data.raw.technology["bob-infinite-worker-robots-storage-1"].effects[1].infer_icon = false
+
+-- TODO: Redesign this to be a function that scans through technology table for a whitelist of effects and then sets infer_icon for those effects
+-- TODO: Move tech effect icon overwrites to its own function/file, along with the above
