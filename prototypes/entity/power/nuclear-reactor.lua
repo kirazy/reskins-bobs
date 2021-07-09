4164ad0c4568ef0107d425bdbb6f734a12060ed6
Kira <35714562+kirazy@users.noreply.github.com>
2021-07-09 11:15:33 -0700

    Squashed commit of the following:
    commit 456794c56e6c78918950cf8c02c60e9db812be62
    Author: Kira <35714562+kirazy@users.noreply.github.com>
    Date:   Fri Jul 9 11:01:44 2021 -0700
    
        Update changelog.txt
    
    commit 4341d7a9fe9629982f5c06df15213f955387f31b
    Author: Kira <35714562+kirazy@users.noreply.github.com>
    Date:   Fri Jul 9 10:50:34 2021 -0700
    
        Fix tint, make trigger-based checks for various entities

--- prototypes/entity/power/nuclear-reactor.lua
+++ prototypes/entity/power/nuclear-reactor.lua
@@ -5,7 +5,7 @@
 
 -- Check to see if reskinning needs to be done.
 if not (reskins.bobs and reskins.bobs.triggers.power.entities) then return end
-if reskins.lib.setting("bobmods-power-nuclear") == false then return end
+if not (reskins.bobs and reskins.bobs.triggers.power.nuclear) then return end
 
 -- Set input parameters
 local inputs = {
