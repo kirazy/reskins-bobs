65ff7de7610b5c3bd7448e7c20ba03db96272feb
snouz <snooz-x@hotmail.com>
2021-12-20 15:38:40 +0100

    Recipe groups icons

---
+++ prototypes/item-group.lua
@@ -0,0 +1,30 @@
+if data.raw["item-group"]["bob-logistics"] and data.raw["item-group"]["logistics"] then
+  data.raw["item-group"]["bob-logistics"].icon = "__reskins-bobs__/graphics/item-group/bob-logistics.png"
+  data.raw["item-group"]["bob-logistics"].icon_size = 128
+
+  data.raw["item-group"]["logistics"].icon = "__reskins-bobs__/graphics/item-group/logistics.png"
+  data.raw["item-group"]["logistics"].icon_size = 128
+end
+
+if data.raw["item-group"]["bob-fluid-products"] then
+  data.raw["item-group"]["bob-fluid-products"].icon = "__reskins-bobs__/graphics/item-group/bob-fluid-products.png"
+  data.raw["item-group"]["bob-fluid-products"].icon_size = 128
+end
+
+if data.raw["item-group"]["bob-resource-products"] then
+  data.raw["item-group"]["bob-resource-products"].icon = "__reskins-bobs__/graphics/item-group/bob-resource-products.png"
+  data.raw["item-group"]["bob-resource-products"].icon_size = 128
+end
+
+if data.raw["item-group"]["bob-intermediate-products"] and data.raw["item-group"]["intermediate-products"] then
+  data.raw["item-group"]["bob-intermediate-products"].icon = "__reskins-bobs__/graphics/item-group/bob-intermediate-products.png"
+  data.raw["item-group"]["bob-intermediate-products"].icon_size = 128
+
+  data.raw["item-group"]["intermediate-products"].icon = "__reskins-bobs__/graphics/item-group/intermediate-products.png"
+  data.raw["item-group"]["intermediate-products"].icon_size = 128
+end
+
+if data.raw["item-group"]["bob-gems"] then
+  data.raw["item-group"]["bob-gems"].icon = "__reskins-bobs__/graphics/item-group/bob-gems.png"
+  data.raw["item-group"]["bob-gems"].icon_size = 128
+end
