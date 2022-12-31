bb232cfa38ccd401b3dba72558219d91d170a6dd
Kira <35714562+kirazy@users.noreply.github.com>
2022-12-30 16:09:16 -0800

    Add support for nuclear reactor technologies

--- prototypes/technology/power-updates.lua
+++ prototypes/technology/power-updates.lua
@@ -19,14 +19,28 @@ local technologies = {
     ["bob-steam-engine-1"] = {tier = 1, icon_name = "steam-engine", technology_icon_size = 128, technology_icon_mipmaps = 0}, -- Bob technology burner phase
 }
 
+local material_tiers = {
+    "base",
+    "silver-aluminum",
+    "gold-copper",
+}
+
+if reskins.lib.migration.is_version_or_newer(mods["bobpower"], "1.1.6") then
+    material_tiers = {
+        "aluminum-invar",
+        "silver-titanium",
+        "gold-copper",
+    }
+end
+
 -- Nuclear reactors
 if reskins.bobs.triggers.power.nuclear then
     technologies["nuclear-power"] = {tier = 1, prog_tier = 3, icon_name = "nuclear-power", tint = reskins.bobs.nuclear_reactor_index["nuclear-reactor"].tint} -- t3 reactor
-    technologies["nuclear-power"].icon_base = "nuclear-power-uranium-1"
+    technologies["nuclear-power"].icon_base = "nuclear-power-uranium-"..material_tiers[1]
     technologies["bob-nuclear-power-2"] = {tier = 2, prog_tier = 4, icon_name = "nuclear-power", tint = reskins.bobs.nuclear_reactor_index["nuclear-reactor-2"].tint} -- t4 reactor
-    technologies["bob-nuclear-power-2"].icon_base = "nuclear-power-uranium-2"
+    technologies["bob-nuclear-power-2"].icon_base = "nuclear-power-uranium-"..material_tiers[2]
     technologies["bob-nuclear-power-3"] = {tier = 3, prog_tier = 5, icon_name = "nuclear-power", tint = reskins.bobs.nuclear_reactor_index["nuclear-reactor-3"].tint} -- t5 reactor
-    technologies["bob-nuclear-power-3"].icon_base = "nuclear-power-uranium-3"
+    technologies["bob-nuclear-power-3"].icon_base = "nuclear-power-uranium-"..material_tiers[3]
 
     if reskins.lib.setting("bobmods-revamp-nuclear") == true then
         technologies["bob-nuclear-power-2"].icon_base = "nuclear-power-thorium-2"
