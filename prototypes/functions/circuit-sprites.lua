6964cd427909479f6c777fa784ec67366370d523
Kira <kirazy@live.com>
2025-03-29 14:36:18 -0700

    fix: remove unused flags property, #60

--- prototypes/functions/circuit-sprites.lua
+++ prototypes/functions/circuit-sprites.lua
@@ -12,50 +12,49 @@ local circuit_names = {
 }
 
 for tier, circuit_name in pairs(circuit_names) do
-	-- Make the standard sprites.
-	data:extend({
-		{
-			type = "sprite",
-			name = "ar-" .. circuit_name .. "-standard",
-			filename = "__reskins-bobs__/graphics/icons/sprites/circuits/standard/" .. circuit_name .. ".png",
-			size = 40,
-			mipmap_count = 2,
-			flags = { "gui-icon" },
-		},
-	})
+	---@type data.SpritePrototype
+	local standard_sprite = {
+		type = "sprite",
+		name = "ar-" .. circuit_name .. "-standard",
+		filename = "__reskins-bobs__/graphics/icons/sprites/circuits/standard/" .. circuit_name .. ".png",
+		size = 40,
+		mipmap_count = 2,
+		flags = { "gui-icon" },
+	}
 
-	-- Fetch tint, ensure it has no alpha; circuits do not support partial transparency.
+	data:extend({ standard_sprite })
+
+	-- Ensure tint has no alpha; circuit coloring does not support partial transparency.
 	local tint = reskins.lib.tiers.get_tint(tier)
 	local sanitized_tint = util.get_color_with_alpha(tint, 1)
 
-	-- Make tier colored sprites
-	data:extend({
-		{
-			type = "sprite",
-			name = "ar-" .. circuit_name .. "-colored",
-			layers = {
-				{
-					filename = "__reskins-bobs__/graphics/icons/sprites/circuits/colored/" .. circuit_name .. "/" .. circuit_name .. "-base.png",
-					size = 40,
-					tint = sanitized_tint,
-					mipmap_count = 2,
-					flags = { "gui-icon" },
-				},
-				{
-					filename = "__reskins-bobs__/graphics/icons/sprites/circuits/colored/" .. circuit_name .. "/" .. circuit_name .. "-highlights.png",
-					size = 40,
-					blend_mode = "additive",
-					mipmap_count = 2,
-					flags = { "gui-icon" },
-				},
-				{
-					filename = "__reskins-bobs__/graphics/icons/sprites/circuits/colored/" .. circuit_name .. "/" .. circuit_name .. "-traces.png",
-					size = 40,
-					mipmap_count = 2,
-					flags = { "gui-icon" },
-				},
+	---@type data.SpritePrototype
+	local colored_sprite = {
+		type = "sprite",
+		name = "ar-" .. circuit_name .. "-colored",
+		layers = {
+			{
+				filename = "__reskins-bobs__/graphics/icons/sprites/circuits/colored/" .. circuit_name .. "/" .. circuit_name .. "-base.png",
+				size = 40,
+				tint = sanitized_tint,
+				mipmap_count = 2,
+				flags = { "gui-icon" },
 			},
-			flags = { "gui-icon" },
+			{
+				filename = "__reskins-bobs__/graphics/icons/sprites/circuits/colored/" .. circuit_name .. "/" .. circuit_name .. "-highlights.png",
+				size = 40,
+				blend_mode = "additive",
+				mipmap_count = 2,
+				flags = { "gui-icon" },
+			},
+			{
+				filename = "__reskins-bobs__/graphics/icons/sprites/circuits/colored/" .. circuit_name .. "/" .. circuit_name .. "-traces.png",
+				size = 40,
+				mipmap_count = 2,
+				flags = { "gui-icon" },
+			},
 		},
-	})
+	}
+
+	data:extend({ colored_sprite })
 end
