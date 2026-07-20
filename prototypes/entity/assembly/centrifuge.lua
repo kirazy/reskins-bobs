3a9169f005a0a9f3afb5661f7c4a2f2a304dd333
Kira <kirazy@live.com>
2026-07-19 20:08:56 -0700

    fix: use centrifuge sprites from asset mod
    The centrifuge sprites have been re-rendered but otherwise remain unchanged. In the near term, leverage the asset mod for this.

--- prototypes/entity/assembly/centrifuge.lua
+++ prototypes/entity/assembly/centrifuge.lua
@@ -46,7 +46,7 @@ for name, map in pairs(tier_map) do
 		layers = {
 			-- Base
 			{
-				filename = "__base__/graphics/entity/centrifuge/remnants/centrifuge-remnants.png",
+				filename = "__reskins-assets-base__/graphics/entity/centrifuge/remnants/centrifuge-remnants-base.png",
 				width = 286,
 				height = 284,
 				direction_count = 1,
@@ -55,7 +55,7 @@ for name, map in pairs(tier_map) do
 			},
 			-- Mask
 			{
-				filename = "__reskins-bobs__/graphics/entity/assembly/centrifuge/remnants/centrifuge-remnants-mask.png",
+				filename = "__reskins-assets-base__/graphics/entity/centrifuge/remnants/centrifuge-remnants-mask.png",
 				width = 286,
 				height = 284,
 				direction_count = 1,
@@ -65,7 +65,7 @@ for name, map in pairs(tier_map) do
 			},
 			-- Highlights
 			{
-				filename = "__reskins-bobs__/graphics/entity/assembly/centrifuge/remnants/centrifuge-remnants-highlights.png",
+				filename = "__reskins-assets-base__/graphics/entity/centrifuge/remnants/centrifuge-remnants-highlights.png",
 				width = 286,
 				height = 284,
 				direction_count = 1,
@@ -76,266 +76,199 @@ for name, map in pairs(tier_map) do
 		},
 	})
 
-	-- Reskin entities
-	entity.graphics_set.idle_animation = {
-		layers = {
-			-- Centrifuge C
-			{
-				filename = "__base__/graphics/entity/centrifuge/centrifuge-C.png",
-				priority = "high",
-				scale = 0.5,
-				line_length = 8,
-				width = 237,
-				height = 214,
-				frame_count = 64,
-				shift = util.by_pixel(-0.25, -26.5),
-			},
-			{
-				filename = "__base__/graphics/entity/centrifuge/centrifuge-C-shadow.png",
-				draw_as_shadow = true,
-				priority = "high",
-				scale = 0.5,
-				line_length = 8,
-				width = 279,
-				height = 152,
-				frame_count = 64,
-				shift = util.by_pixel(16.75, -10),
-			},
-			-- Centrifuge B
-			{
-				filename = "__base__/graphics/entity/centrifuge/centrifuge-B.png",
-				priority = "high",
-				scale = 0.5,
-				line_length = 8,
-				width = 156,
-				height = 234,
-				frame_count = 64,
-				shift = util.by_pixel(23, 6.5),
-			},
-			{
-				filename = "__reskins-bobs__/graphics/entity/assembly/centrifuge/centrifuge-B-mask.png",
-				priority = "high",
-				tint = inputs.tint,
-				scale = 0.5,
-				line_length = 8,
-				width = 156,
-				height = 234,
-				frame_count = 64,
-				shift = util.by_pixel(23, 6.5),
-			},
-			{
-				filename = "__reskins-bobs__/graphics/entity/assembly/centrifuge/centrifuge-B-highlights.png",
-				priority = "high",
-				blend_mode = reskins.lib.settings.blend_mode, -- "additive",
-				scale = 0.5,
-				line_length = 8,
-				width = 156,
-				height = 234,
-				frame_count = 64,
-				shift = util.by_pixel(23, 6.5),
-			},
-			{
-				filename = "__base__/graphics/entity/centrifuge/centrifuge-B-shadow.png",
-				draw_as_shadow = true,
-				priority = "high",
-				scale = 0.5,
-				line_length = 8,
-				width = 251,
-				height = 149,
-				frame_count = 64,
-				shift = util.by_pixel(63.25, 15.25),
-			},
-			-- Centrifuge A
-			{
-				filename = "__base__/graphics/entity/centrifuge/centrifuge-A.png",
-				priority = "high",
-				scale = 0.5,
-				line_length = 8,
-				width = 139,
-				height = 246,
-				frame_count = 64,
-				shift = util.by_pixel(-26.25, 3.5),
-			},
-			{
-				filename = "__reskins-bobs__/graphics/entity/assembly/centrifuge/centrifuge-A-mask.png",
-				priority = "high",
-				tint = inputs.tint,
-				scale = 0.5,
-				line_length = 8,
-				width = 139,
-				height = 246,
-				frame_count = 64,
-				shift = util.by_pixel(-26.25, 3.5),
-			},
-			{
-				filename = "__reskins-bobs__/graphics/entity/assembly/centrifuge/centrifuge-A-highlights.png",
-				priority = "high",
-				blend_mode = reskins.lib.settings.blend_mode, -- "additive",
-				scale = 0.5,
-				line_length = 8,
-				width = 139,
-				height = 246,
-				frame_count = 64,
-				shift = util.by_pixel(-26.25, 3.5),
-			},
-			{
-				filename = "__base__/graphics/entity/centrifuge/centrifuge-A-shadow.png",
-				priority = "high",
-				draw_as_shadow = true,
-				scale = 0.5,
-				line_length = 8,
-				width = 230,
-				height = 124,
-				frame_count = 64,
-				shift = util.by_pixel(8.5, 23.5),
+	entity.graphics_set = {
+		always_draw_idle_animation = true,
+		idle_animation = {
+			layers = {
+				-- Centrifuge C
+				{
+					filename = "__reskins-assets-base__/graphics/entity/centrifuge/centrifuge-c-base.png",
+					priority = "high",
+					scale = 0.5,
+					line_length = 8,
+					width = 237,
+					height = 214,
+					frame_count = 64,
+					shift = util.by_pixel(-0.25, -26.5),
+				},
+				{
+					filename = "__reskins-assets-base__/graphics/entity/centrifuge/centrifuge-c-shadow.png",
+					draw_as_shadow = true,
+					priority = "high",
+					scale = 0.5,
+					line_length = 8,
+					width = 279,
+					height = 152,
+					frame_count = 64,
+					shift = util.by_pixel(16.75, -10),
+				},
+				-- Centrifuge B
+				{
+					filename = "__reskins-assets-base__/graphics/entity/centrifuge/centrifuge-b-base.png",
+					priority = "high",
+					scale = 0.5,
+					line_length = 8,
+					width = 156,
+					height = 234,
+					frame_count = 64,
+					shift = util.by_pixel(23, 6.5),
+				},
+				{
+					filename = "__reskins-assets-base__/graphics/entity/centrifuge/centrifuge-b-mask.png",
+					priority = "high",
+					tint = inputs.tint,
+					scale = 0.5,
+					line_length = 8,
+					width = 156,
+					height = 234,
+					frame_count = 64,
+					shift = util.by_pixel(23, 6.5),
+				},
+				{
+					filename = "__reskins-assets-base__/graphics/entity/centrifuge/centrifuge-b-highlights.png",
+					priority = "high",
+					blend_mode = reskins.lib.settings.blend_mode,
+					scale = 0.5,
+					line_length = 8,
+					width = 156,
+					height = 234,
+					frame_count = 64,
+					shift = util.by_pixel(23, 6.5),
+				},
+				{
+					filename = "__reskins-assets-base__/graphics/entity/centrifuge/centrifuge-b-shadow.png",
+					draw_as_shadow = true,
+					priority = "high",
+					scale = 0.5,
+					line_length = 8,
+					width = 251,
+					height = 149,
+					frame_count = 64,
+					shift = util.by_pixel(63.25, 15.25),
+				},
+				-- Centrifuge A
+				{
+					filename = "__reskins-assets-base__/graphics/entity/centrifuge/centrifuge-a-base.png",
+					priority = "high",
+					scale = 0.5,
+					line_length = 8,
+					width = 139,
+					height = 246,
+					frame_count = 64,
+					shift = util.by_pixel(-26.25, 3.5),
+				},
+				{
+					filename = "__reskins-assets-base__/graphics/entity/centrifuge/centrifuge-a-mask.png",
+					priority = "high",
+					tint = inputs.tint,
+					scale = 0.5,
+					line_length = 8,
+					width = 139,
+					height = 246,
+					frame_count = 64,
+					shift = util.by_pixel(-26.25, 3.5),
+				},
+				{
+					filename = "__reskins-assets-base__/graphics/entity/centrifuge/centrifuge-a-highlights.png",
+					priority = "high",
+					blend_mode = reskins.lib.settings.blend_mode,
+					scale = 0.5,
+					line_length = 8,
+					width = 139,
+					height = 246,
+					frame_count = 64,
+					shift = util.by_pixel(-26.25, 3.5),
+				},
+				{
+					filename = "__reskins-assets-base__/graphics/entity/centrifuge/centrifuge-a-shadow.png",
+					priority = "high",
+					draw_as_shadow = true,
+					scale = 0.5,
+					line_length = 8,
+					width = 230,
+					height = 124,
+					frame_count = 64,
+					shift = util.by_pixel(8.5, 23.5),
+				},
 			},
 		},
-	}
-
-	entity.graphics_set.working_visualisations = {
-		-- Area Light
-		{
-			effect = "uranium-glow",
-			apply_recipe_tint = "primary",
-			fadeout = true,
-			light = { intensity = 0.1, size = 9.9, shift = { 0.0, 0.0 }, color = { r = 0.0, g = 1.0, b = 0.0 } },
-		},
-
-		-- Working Light
-		{
-			effect = "uranium-glow",
-			fadeout = true,
-			apply_recipe_tint = "primary",
-			animation = {
-				layers = {
-					-- Centrifuge C
-					{
-						filename = "__reskins-bobs__/graphics/entity/assembly/centrifuge/centrifuge-C-light.png",
-						priority = "high",
-						scale = 0.5,
-						blend_mode = "additive",
-						line_length = 8,
-						width = 190,
-						height = 207,
-						frame_count = 64,
-						shift = util.by_pixel(0, -27.25),
-						draw_as_glow = true,
-					},
-					-- Centrifuge B
-					{
-						filename = "__reskins-bobs__/graphics/entity/assembly/centrifuge/centrifuge-B-light.png",
-						priority = "high",
-						scale = 0.5,
-						blend_mode = "additive",
-						line_length = 8,
-						width = 131,
-						height = 206,
-						frame_count = 64,
-						shift = util.by_pixel(16.75, 0.5),
-						draw_as_glow = true,
-					},
-					-- Centrifuge A
-					{
-						filename = "__reskins-bobs__/graphics/entity/assembly/centrifuge/centrifuge-A-light.png",
-						priority = "high",
-						scale = 0.5,
-						blend_mode = "additive",
-						line_length = 8,
-						width = 108,
-						height = 197,
-						frame_count = 64,
-						shift = util.by_pixel(-23.5, -1.75),
-						draw_as_glow = true,
+		working_visualisations = {
+			-- Area Light
+			{
+				effect = "uranium-glow",
+				apply_recipe_tint = "primary",
+				fadeout = true,
+				light = {
+					intensity = 0.1,
+					size = 9.9,
+					shift = { 0.0, 0.0 },
+					color = { r = 0.0, g = 1.0, b = 0.0 },
+				},
+			},
+			-- Working Light
+			{
+				effect = "uranium-glow",
+				fadeout = true,
+				apply_recipe_tint = "primary",
+				animation = {
+					layers = {
+						-- Centrifuge C
+						{
+							filename = "__reskins-assets-base__/graphics/entity/centrifuge/lights/centrifuge-c-light.png",
+							priority = "high",
+							scale = 0.5,
+							blend_mode = "additive",
+							line_length = 8,
+							width = 190,
+							height = 207,
+							frame_count = 64,
+							shift = util.by_pixel(0, -27.25),
+							draw_as_glow = true,
+						},
+						-- Centrifuge B
+						{
+							filename = "__reskins-assets-base__/graphics/entity/centrifuge/lights/centrifuge-b-light.png",
+							priority = "high",
+							scale = 0.5,
+							blend_mode = "additive",
+							line_length = 8,
+							width = 131,
+							height = 206,
+							frame_count = 64,
+							shift = util.by_pixel(16.75, 0.5),
+							draw_as_glow = true,
+						},
+						-- Centrifuge A
+						{
+							filename = "__reskins-assets-base__/graphics/entity/centrifuge/lights/centrifuge-a-light.png",
+							priority = "high",
+							scale = 0.5,
+							blend_mode = "additive",
+							line_length = 8,
+							width = 108,
+							height = 197,
+							frame_count = 64,
+							shift = util.by_pixel(-23.5, -1.75),
+							draw_as_glow = true,
+						},
 					},
 				},
 			},
+		},
+		water_reflection = {
+			pictures = {
+				filename = "__reskins-assets-base__/graphics/entity/centrifuge/centrifuge-reflection.png",
+				priority = "extra-high",
+				width = 28,
+				height = 32,
+				shift = util.by_pixel(0, 65),
+				variation_count = 1,
+				scale = 5,
+			},
+			rotate = false,
+			orientation_to_variation = false,
 		},
 	}
 
 	::continue::
 end
-
--- entity.graphics_set.working_visualisations = {
---     -- WORKING LIGHTS
---     -- Centrifuge A (Front Left)
---     {
---         effect = "uranium-glow",
---         fadeout = true,
---         apply_recipe_tint = "primary",
---         animation = {
---             filename = "__reskins-bobs__/graphics/entity/assembly/centrifuge/centrifuge-A-light.png",
---             priority = "high",
---             scale = 0.5,
---             blend_mode = "additive",
---             line_length = 8,
---             width = 108,
---             height = 197,
---             frame_count = 64,
---             shift = util.by_pixel(-23.5, -1.75),
---             draw_as_glow = true,
---         },
---     },
-
---     -- Centrifuge B (Front Right)
---     {
---         effect = "uranium-glow",
---         fadeout = true,
---         apply_recipe_tint = "secondary",
---         animation = {
---             filename = "__reskins-bobs__/graphics/entity/assembly/centrifuge/centrifuge-B-light.png",
---             priority = "high",
---             scale = 0.5,
---             blend_mode = "additive",
---             line_length = 8,
---             width = 131,
---             height = 206,
---             frame_count = 64,
---             shift = util.by_pixel(16.75, 0.5),
---             draw_as_glow = true,
---         },
---     },
-
---     -- Centrifuge C (Rear)
---     {
---         effect = "uranium-glow",
---         fadeout = true,
---         apply_recipe_tint = "tertiary",
---         animation = {
---             filename = "__reskins-bobs__/graphics/entity/assembly/centrifuge/centrifuge-C-light.png",
---             priority = "high",
---             scale = 0.5,
---             blend_mode = "additive",
---             line_length = 8,
---             width = 190,
---             height = 207,
---             frame_count = 64,
---             shift = util.by_pixel(0, -27.25),
---             draw_as_glow = true,
---         }
---     },
-
---     -- AREA LIGHT
---     -- Centrifuge A (Front Left)
---     {
---         effect = "uranium-glow",
---         apply_recipe_tint = "primary",
---         fadeout = true,
---         light = {intensity = 0.066, size = 8.9, shift = {-1, 0.5}}
---     },
-
---     -- Centrifuge B (Front Right)
---     {
---         effect = "uranium-glow",
---         apply_recipe_tint = "secondary",
---         fadeout = true,
---         light = {intensity = 0.066, size = 8.9, shift = {1, 0.5}}
---     },
-
---     -- Centrifuge C (Rear)
---     {
---         effect = "uranium-glow",
---         apply_recipe_tint = "tertiary",
---         fadeout = true,
---         light = {intensity = 0.066, size = 8.9, shift = {0, -1.2}}
---     },
--- }
